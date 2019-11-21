33bdaea0a70c13d9ca5b5f6fe89f5b8c5b879d66
Kyle Robbertze <kyle@paddatrapper.com>
2019-11-21 17:39:03 +0200

    Add migration to backfill organizations

---
+++ seed/migrations/0114_backfill_organization_columns.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models, transaction
+
+def fill(apps, schema_editor):
+    Column = apps.get_model("seed", "Column")
+    Organization = apps.get_model("orgs", "Organization")
+    with transaction.atomic():
+        orgs = Organization.objects.filter(parent_org=None).order_by('id')
+        for org in orgs:
+            sub_orgs = Organization.objects.filter(parent_org=org.pk)
+            for sub_org in sub_orgs:
+                columns = Column.objects.filter(organization=sub_org.pk)
+                for column in columns:
+                    if Column.objects.filter(organization=org.pk,
+                                              column_name=column.column_name).count() > 0:
+                        break
+                    parent_col = Column.objects.get(pk=column.pk)
+                    parent_col.pk = None
+                    parent_col.organization = org
+                    parent_col.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('seed', '0113_column_geocoding_order'),
+    ]
+
+    operations = [
+        migrations.RunPython(fill)
+    ]
