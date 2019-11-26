e05b4ff915b78260c1846551f03ac9df10826f45
Kyle Robbertze <kyle@paddatrapper.com>
2019-11-26 10:58:12 +0200

    fix flake8 formatting

--- seed/migrations/0114_backfill_organization_columns.py
+++ seed/migrations/0114_backfill_organization_columns.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models, transaction
+from django.db import migrations, transaction
+
 
 def fill(apps, schema_editor):
     Column = apps.get_model("seed", "Column")
@@ -13,13 +14,13 @@ def fill(apps, schema_editor):
             for sub_org in sub_orgs:
                 columns = Column.objects.filter(organization=sub_org.pk)
                 for column in columns:
-                    if Column.objects.filter(organization=org.pk,
-                                              column_name=column.column_name).count() > 0:
+                    if Column.objects.filter(organization=org.pk, column_name=column.column_name).count() > 0:
                         break
                     parent_col = Column.objects.get(pk=column.pk)
                     parent_col.pk = None
                     parent_col.organization = org
                     parent_col.save()
+
 
 class Migration(migrations.Migration):
 
